7937a4735bd68ff6f837e65f5c26df7bc92028b4
dingyi222666@foxmail.com <dingyi222666@foxmail.com>
2022-06-08 03:57:41 +0800

    fix: pull github code fail

--- vercel.sh
+++ vercel.sh
@@ -4,6 +4,8 @@ export TZ='Asia/Shanghai'
 
 hexo clean
 
+git config submodule.themes/butterfly.url https://github.com/jerryc127/hexo-theme-butterfly.git 
+
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
