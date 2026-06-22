a521d81a572e28bf7906d0db289840b96322dd0a
dingyi <dingyi222666@foxmail.com>
2026-06-23 02:53:24 +0800

    build: update Hexo and Volantis theme
    Update Hexo to 8.1.2 and Volantis 6.x to v6.0.4. Sync Volantis config and switch blog fonts to Maple Mono.

--- vercel.sh
+++ vercel.sh
@@ -6,13 +6,9 @@ hexo clean
 
 git config submodule.themes/volantis.url https://github.com/volantis-x/hexo-theme-volantis.git
 
-git submodule update --init --recursive
-git submodule update --recursive --remote
-
-cd themes/volantis
-git checkout 6.0
-
-cd ../..
+git submodule sync -- themes/volantis
+git submodule update --init --recursive -- themes/volantis
+git submodule update --remote --checkout --recursive -- themes/volantis
 
 npm install
 
