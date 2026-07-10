b2e1a80d52497eb5a1140db226e265523d39281e
dingyi <dingyi222666@foxmail.com>
2026-07-10 23:50:22 +0800

    fix: use HTTPS for theme submodules so Vercel can clone
    SSH submodule URLs fail host key verification on Vercel, so Volantis never loads and tag plugins like folding break the build.

--- vercel.sh
+++ vercel.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+set -e
 
 export TZ='Asia/Shanghai'
 
 hexo clean
 
-git config submodule.themes/volantis.url https://github.com/volantis-x/hexo-theme-volantis.git
-
 git submodule sync -- themes/volantis
-git submodule update --init --recursive -- themes/volantis
-git submodule update --remote --checkout --recursive -- themes/volantis
+git submodule update --init --recursive --remote -- themes/volantis
 
 npm install
 
